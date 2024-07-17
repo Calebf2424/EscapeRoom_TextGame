7f510d813a76adc2f97ed23b03cb391c9032b282
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-07-17 14:24:38 -0600

    Added new rooms to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
-//test
-//toBeChanged
 #include "Room.h"
 #include "Item.h"
 
 int main() {
     // Create rooms with descriptions, lock keys, and items
     Cell* cell1 = new Cell("You are in a dark cell with damp walls.", 1234);
+    Hallway* hallway = new Hallway("You find yourself in a narrow hallway with flickering lights.", "open");
+    Cave* cave = new Cave("You enter a vast cave with echoes of dripping water.");
 
     // Create RunRoom object and add rooms
     RunRoom game;
     game.addRoom(cell1);
+    game.addRoom(hallway);
+    game.addRoom(cave);
 
     // Start the game
     game.start();
 
     // Cleanup dynamically allocated memory
     delete cell1;
+    delete hallway;
+    delete cave;
 
     return 0;
 }
