c2ad1490d446ece0774e0ad7d2e90e5c6a646ca6
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-07-17 22:56:25 -0600

    Update BuildProjectCurrent.bat

--- BuildProjectCurrent.bat
+++ BuildProjectCurrent.bat
@@ -1,22 +1,8 @@
 @echo off
 setlocal
 
-:: Prompt user for file path
-set /p filepath="Enter the path to your project directory: "
-
-:: Check if the path exists
-if not exist "%filepath%" (
-    echo The specified path does not exist.
-    pause
-    exit /b 1
-)
-
 :: Change to the specified directory
-cd "%filepath%" || (
-    echo Failed to change to the specified directory.
-    pause
-    exit /b 1
-)
+cd C:\Users\cfarn\EscapeRoom_TextGame
 
 :: Remove the build directory if it exists
 rmdir /s /q build
@@ -29,7 +15,7 @@ mkdir build
 cd build
 
 :: Run CMake commands
-cmake .
+cmake ..
 cmake --build .
 
 pause
