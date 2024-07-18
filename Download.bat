0aa6a63ba441f84fe2c60e3a1c40237f6a005818
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-07-18 10:01:57 -0600

    Create Download.bat

---
+++ Download.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM Check if running in administrative mode
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto Admin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0""", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+:Admin
+
+REM Switch to the Program Files directory
+cd "C:\Program Files"
+
+REM Clone the repository
+git clone https://github.com/Calebf2424/EscapeRoom_TextGame.git
+
+pause
